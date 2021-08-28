7a1f78110e1e3062309404d64d738e1f500df4eb
Proxlight <69844328+Proxlight@users.noreply.github.com>
2021-08-28 14:30:11 +0530

    Add files via upload

--- Weather Forecast.py
+++ Weather Forecast.py
@@ -1,8 +1,13 @@
+# Importing Packages
 import weather_forecast as wf
 
+# Taking user's location as an input
 location=input("Enter Location Here :")
-print('Displaying Weather report for: ' + location)#output:
 
+print('Displaying Weather report for: ' + location)
+
+# Retriving the data using .forecast() method
 Weather= wf.forecast(place = location)
 
+# Printing the final results
 print(Weather)
